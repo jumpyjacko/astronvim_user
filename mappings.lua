0d8096dbcc7d7a6f1cbc85c39391c8fdc9f52fe7
JumpyJacko <bobjacklee27@gmail.com>
2023-04-13 12:41:43 +1000

    Added my colemak bindings

--- mappings.lua
+++ mappings.lua
@@ -20,6 +20,73 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- My Beloved vim-colemak Bindings
+    -- Movement Keys
+    ["h"] = { "h", desc = "Move left" },
+    ["n"] = { "j", desc = "Move down" },
+    ["e"] = { "k", desc = "Move up" },
+    ["i"] = { "l", desc = "Move right" },
+    -- Words forward/backward
+    ["l"] = { "b", desc = "Move to start of word" },
+    ["L"] = { "B", desc = "Move to start of word" },
+    ["u"] = { "e", desc = "Move to end of word" },
+    ["U"] = { "E", desc = "Move to end of word" },
+    ["y"] = { "w", desc = "Move to end of word" },
+    ["Y"] = { "W", desc = "Move to end of word" },
+
+    -- Mode Keys
+    ["s"] =     { "i",     desc = "Insert mode" },
+    ["S"] =     { "I",     desc = "Insert mode (Start of line)" },
+    ["t"] =     { "a",     desc = "Append mode" },
+    ["T"] =     { "A",     desc = "Append mode (End of line)" },
+    ["a"] =     { "v",     desc = "Visual mode" },
+    ["A"] =     { "V",     desc = "Visual line mode" },
+    ["<C-a>"] = { "<C-v>", desc = "Visual block mode" },
+    ["ga"] =    { "gv",    desc = "Visual mode (?)" },
+
+    -- Cut/Copy/Paste and Undo/Redo
+    ["x"] =  { "x",     desc = "Cut" },
+    ["c"] =  { "y",     desc = "Copy" },
+    ["v"] =  { "p",     desc = "Paste" },
+    ["X"] =  { "dd",    desc = "Cut line"},
+    ["C"] =  { "yy",    desc = "Copy line"},
+    ["V"] =  { "P",     desc = "Paste on current line"},
+    ["z"] =  { "u",     desc = "Undo" },
+    ["gz"] = { "U",     desc = "Undo (?)" },
+    ["Z"] =  { "<C-R>", desc = "Redo" },
+
+    -- Change (?)
+    ["w"] = { "c" },
+    ["W"] = { "C" },
+    ["ww"] = { "cc" },
+
+    -- Window Management
+    ["<C-W>h"] = { "<C-W>h" },
+    ["<C-W>n"] = { "<C-W>j" },
+    ["<C-W>e"] = { "<C-W>k" },
+    ["<C-W>i"] = { "<C-W>l" },
+  },
+  i = {
+    ["NE"] = { "<Esc>", desc = "Quick Escape bindings" },
+  },
+  v = {
+    -- Movement Keys
+    ["h"] = { "h", desc = "Move left" },
+    ["n"] = { "j", desc = "Move down" },
+    ["e"] = { "k", desc = "Move up" },
+    ["i"] = { "l", desc = "Move right" },
+    
+    -- Cut/Copy/Paste and Undo/Redo
+    ["x"] =  { "x",     desc = "Cut" },
+    ["c"] =  { "y",     desc = "Copy" },
+    ["v"] =  { "p",     desc = "Paste" },
+    ["X"] =  { "dd",    desc = "Cut line"},
+    ["C"] =  { "yy",    desc = "Copy line"},
+    ["V"] =  { "P",     desc = "Paste on current line"},
+    ["z"] =  { "u",     desc = "Undo" },
+    ["gz"] = { "U",     desc = "Undo (?)" },
+    ["Z"] =  { "<C-R>", desc = "Redo" },
   },
   t = {
     -- setting a mapping to false will disable it
