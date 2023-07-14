3d57ae62bd2143837278e736a8c746643ee871bd
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2023-07-14 15:39:27 +1000

    Fixed rainbow-delimiters

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,6 +7,6 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.nvim-regexplainer"},
-  { import = "astrocommunity.editing-support.rainbow-delimiters" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.media.presence-nvim"},
 }
