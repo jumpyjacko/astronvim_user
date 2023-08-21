210203f98bb773a0754210e055fce9d0586dccc0
JumpyJacko <bobjacklee27@gmail.com>
2023-08-21 21:35:12 +1000

    Changed to catppuccin colourscheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.editing-support.nvim-regexplainer"},
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.media.presence-nvim"},
